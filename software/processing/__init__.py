b10030078ead4a3d229a984241ec949df394136f
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-08 07:43:29 +0900

    Add processing package __init__.py file

---
+++ software/processing/__init__.py
@@ -0,0 +1,34 @@
+"""
+Processing package for the Integrated Electrical-Thermal Impedance Analyzer
+
+This package contains modules for processing and analyzing electrical and 
+thermal impedance data, including signal processing, noise reduction, and
+equivalent circuit modeling.
+
+Modules:
+- signal_processing: Core signal processing utilities
+- electrical_models: Electrical equivalent circuit models
+- thermal_models: Thermal equivalent circuit models
+- noise_reduction: Noise filtering and reduction techniques
+
+Based on the methodology described in the patent:
+열 임피던스와 전기 임피던스 통합 분석 시스템 및 방법
+(Integrated Electrical-Thermal Impedance Analysis System and Method)
+
+Author: Jihwan Jang
+Organization: Ucaretron Inc.
+"""
+
+# These imports will be activated when the respective modules are implemented
+# from .signal_processing import (process_impedance_data, detrend_data, 
+#                               smooth_data, extract_features)
+# from .electrical_models import (RandlesCircuit, RCCircuit, RLCCircuit,
+#                               ColeColeModel, ConstantPhaseElement)
+# from .thermal_models import (FosterNetwork, CauerNetwork,
+#                            DistributedParameterModel)
+# from .noise_reduction import (adaptive_filter, wavelet_denoise, 
+#                             kalman_filter, median_filter)
+
+__all__ = [
+    # Will be updated as modules are implemented
+]
