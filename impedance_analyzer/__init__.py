4a89aa115be1be176cdd8b1c43bdbb26a3da1e5a
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-08 08:50:27 +0900

    Update __init__.py to import all modules

--- impedance_analyzer/__init__.py
+++ impedance_analyzer/__init__.py
@@ -1,13 +1,29 @@
 """
 Integrated Electrical-Thermal Impedance Analysis System.
 
-This package provides tools for simultaneous acquisition and analysis 
-of electrical and thermal impedance data for comprehensive 
-characterization of various systems.
+This package provides tools for simultaneous acquisition and analysis of
+electrical and thermal impedance data for comprehensive system characterization.
 """
 
 from ._version import __version__
 
+# Import key components
 from .analyzer import IntegratedImpedanceAnalyzer
+from .electrical_impedance import ElectricalImpedanceAnalyzer, MeasurementMode
+from .thermal_impedance import ThermalImpedanceAnalyzer, ThermalStimulationMode
+from .thermal_management import PCMThermalManager, PCMType, ThermalEnhancerType
+from .ai_analyzer import AIAnalyzer, ImpedanceDataset
 
-__all__ = ['IntegratedImpedanceAnalyzer']
+# Define package exports
+__all__ = [
+    'IntegratedImpedanceAnalyzer',
+    'ElectricalImpedanceAnalyzer',
+    'ThermalImpedanceAnalyzer',
+    'PCMThermalManager',
+    'AIAnalyzer',
+    'ImpedanceDataset',
+    'MeasurementMode',
+    'ThermalStimulationMode',
+    'PCMType',
+    'ThermalEnhancerType',
+]
