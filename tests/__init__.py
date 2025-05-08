093e10fec270b725054a472b5d030a9b7b02fcbd
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-08 11:58:28 +0900

    Add Python package init for tests

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for the Integrated Electrical-Thermal Impedance Analysis System.
+
+This package contains tests to verify the functionality of the system components.
+"""
