c9751837b4929e9ceb87532697fbd80d18858e78
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-08 11:57:06 +0900

    Add Python package init for examples

---
+++ examples/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Examples for the Integrated Electrical-Thermal Impedance Analysis System.
+
+This package contains practical implementation examples that demonstrate
+how to use the system for various applications across different domains.
+"""
+
+__version__ = '0.1.0'
